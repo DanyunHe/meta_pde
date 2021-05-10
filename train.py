791aefb0274af379a2bf24156d4b73b803c0c3f7
DanyunHe <danyunhe@g.harvard.edu>
2021-05-09 23:28:02 -0400

    meta model done

--- train.py
+++ train.py
@@ -13,10 +13,12 @@ def generate_snapshots(duration,num_frame,length,n,func):
 def F(x,t):
   return np.sin(x-t)
 
-model = MetaLearner()
-data=generate_snapshots(1,10000,1,512,F) #10000*512 snapshots
 
-train_loader = torch.utils.data.DataLoader(data)
-meta_system = ExperimentBuilder(model=model,data=data)
+if __name__ == '__main__':
+	model = MetaLearner()
+	data=generate_snapshots(1,10000,1,512,F) #10000*512 snapshots
 
-meta_system.run_experiment()
+	train_loader = torch.utils.data.DataLoader(data)
+	meta_system = ExperimentBuilder(model=model,data=data)
+
+	meta_system.run_experiment()
